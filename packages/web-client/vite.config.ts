2ef7854abacaa5020d931f9c3a66c205f7714316
LiuYang <fyeeme@gmail.com>
2022-06-13 18:10:12 +0800

    fix: disable @intlify/vite-plugin-vue-i18n config cause error compile

--- packages/web-client/vite.config.ts
+++ packages/web-client/vite.config.ts
@@ -6,16 +6,13 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Unocss from 'unocss/vite'
 import { presetIcons, presetWind } from 'unocss'
-
-import path from 'path'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueI18n({
-      include: path.resolve(__dirname, 'locales/**'),
-    }),
+    // vueI18n({
+    //   include: path.resolve(__dirname, 'locales/**'),
+    // }),
     Unocss({
       presets: [presetWind(), presetIcons()],
     }),
